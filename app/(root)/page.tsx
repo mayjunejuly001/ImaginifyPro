614104107e2b3f29d5b5b1e6d7a7f4d57ae7fdeb
mayjunejuly001 <mayankvermacod@gmail.com>
2024-06-01 18:53:33 +0530

    2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import { Collection } from "@/components/shared/Collection"
+// import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
 import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
@@ -32,14 +32,14 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         </ul>
       </section>
 
-      <section className="sm:mt-12">
+      {/* <section className="sm:mt-12">
         <Collection 
           hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
           page={page}
         />
-      </section>
+      </section> */}
     </>
   )
 }

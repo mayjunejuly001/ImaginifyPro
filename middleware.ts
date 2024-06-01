bea19b9a534127b982bc489e18e6a390bee2b5f5
mayjunejuly001 <mayankvermacod@gmail.com>
2024-06-01 19:28:13 +0530

    final1

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
+  publicRoutes: [
+    '/',
+    '/api/webhooks/clerk',
+    '/api/webhooks/stripe',
+    '/transformations/add/recolor',
+    '/transformations/add/restore',
+    '/transformations/add/fill',
+    '/transformations/add/remove',
+    '/transformations/add/removeBackground',
+  ],
 })
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
+      

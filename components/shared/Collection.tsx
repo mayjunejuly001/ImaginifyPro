38419b098a59afcc8a86c09c47d75c6850f51d85
mayjunejuly001 <mayankvermacod@gmail.com>
2024-06-01 19:19:29 +0530

    final

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,23 +1,23 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import Link from "next/link";
-import { useSearchParams, useRouter } from "next/navigation";
-import { CldImage } from "next-cloudinary";
+import Image from 'next/image'
+import Link from 'next/link'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { CldImage } from 'next-cloudinary'
 
 import {
   Pagination,
   PaginationContent,
   PaginationNext,
   PaginationPrevious,
-} from "@/components/ui/pagination";
-import { transformationTypes } from "@/constants";
-import { IImage } from "@/lib/database/models/image.model";
-import { formUrlQuery } from "@/lib/utils";
+} from '@/components/ui/pagination'
+import { transformationTypes } from '@/constants'
+import { IImage } from '@/lib/database/models/image.model'
+import { formUrlQuery } from '@/lib/utils'
 
-import { Button } from "../ui/button";
+import { Button } from '../ui/button'
 
-import { Search } from "./Search";
+import { Search } from './Search'
 
 export const Collection = ({
   hasSearch = false,
@@ -25,91 +25,91 @@ export const Collection = ({
   totalPages = 1,
   page,
 }: {
-  images: IImage[];
-  totalPages?: number;
-  page: number;
-  hasSearch?: boolean;
+  images: IImage[]
+  totalPages?: number
+  page: number
+  hasSearch?: boolean
 }) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+  const router = useRouter()
+  const searchParams = useSearchParams()
 
   // PAGINATION HANDLER
   const onPageChange = (action: string) => {
-    const pageValue = action === "next" ? Number(page) + 1 : Number(page) - 1;
+    const pageValue = action === 'next' ? Number(page) + 1 : Number(page) - 1
 
     const newUrl = formUrlQuery({
       searchParams: searchParams.toString(),
-      key: "page",
+      key: 'page',
       value: pageValue,
-    });
+    })
 
-    router.push(newUrl, { scroll: false });
-  };
+    router.push(newUrl, { scroll: false })
+  }
 
   return (
     <>
-      <div className="collection-heading">
-        <h2 className="h2-bold text-dark-600">Recent Edits</h2>
+      <div className='collection-heading'>
+        <h2 className='h2-bold text-dark-600'>Recent Edits</h2>
         {hasSearch && <Search />}
       </div>
 
       {images.length > 0 ? (
-        <ul className="collection-list">
+        <ul className='collection-list'>
           {images.map((image) => (
-            <Card image={image} key={image._id} />
+            <Card image={image} key={image.id} />
           ))}
         </ul>
       ) : (
-        <div className="collection-empty">
-          <p className="p-20-semibold">Empty List</p>
+        <div className='collection-empty'>
+          <p className='p-20-semibold'>Empty List</p>
         </div>
       )}
 
       {totalPages > 1 && (
-        <Pagination className="mt-10">
-          <PaginationContent className="flex w-full">
+        <Pagination className='mt-10'>
+          <PaginationContent className='flex w-full'>
             <Button
               disabled={Number(page) <= 1}
-              className="collection-btn"
-              onClick={() => onPageChange("prev")}
+              className='collection-btn'
+              onClick={() => onPageChange('prev')}
             >
-              <PaginationPrevious className="hover:bg-transparent hover:text-white" />
+              <PaginationPrevious className='hover:bg-transparent hover:text-white' />
             </Button>
 
-            <p className="flex-center p-16-medium w-fit flex-1">
+            <p className='flex-center p-16-medium w-fit flex-1'>
               {page} / {totalPages}
             </p>
 
             <Button
-              className="button w-32 bg-purple-gradient bg-cover text-white"
-              onClick={() => onPageChange("next")}
+              className='button w-32 bg-purple-gradient bg-cover text-white'
+              onClick={() => onPageChange('next')}
               disabled={Number(page) >= totalPages}
             >
-              <PaginationNext className="hover:bg-transparent hover:text-white" />
+              <PaginationNext className='hover:bg-transparent hover:text-white' />
             </Button>
           </PaginationContent>
         </Pagination>
       )}
     </>
-  );
-};
+  )
+}
 
 const Card = ({ image }: { image: IImage }) => {
   return (
     <li>
-      <Link href={`/transformations/${image._id}`} className="collection-card">
+      <Link href={`/transformations/${image._id}`} className='collection-card'>
         <CldImage
           src={image.publicId}
           alt={image.title}
           width={image.width}
           height={image.height}
           {...image.config}
-          loading="lazy"
-          className="h-52 w-full rounded-[10px] object-cover"
-          sizes="(max-width: 767px) 100vw, (max-width: 1279px) 50vw, 33vw"
+          loading='lazy'
+          className='h-52 w-full rounded-[10px] object-cover'
+          sizes='(max-width: 767px) 100vw, (max-width: 1279px) 50vw, 33vw'
         />
-        <div className="flex-between">
-          <p className="p-20-semibold mr-3 line-clamp-1 text-dark-600">
+        <div className='flex-between'>
+          <p className='p-20-semibold mr-3 line-clamp-1 text-dark-600'>
             {image.title}
           </p>
           <Image
@@ -125,5 +125,5 @@ const Card = ({ image }: { image: IImage }) => {
         </div>
       </Link>
     </li>
-  );
-};
+  )
+}
